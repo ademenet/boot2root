ef9321b1d5713ad3481cdf283bdf165265bc44af
Jonathan LE QUERE <jle-quer@e3r6p17.42.fr>
2017-12-12 17:44:49 +0100

    [MODIFY] Adapt script1.sh

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+echo "ifconfig vmnet8:"
 ifconfig vmnet8 | grep inet | cut -d" " -f2  > vmnet8
 cat vmnet8
-nmap `cat vmnet8`-255
-nmap `cat vmnet8`-255 | grep for | tail -n1 | cut -b 22-34 > vmnet8_port
+echo "\nnmap `cat vmnet8`-255:"
+nmap `cat vmnet8`-255 > vmnet8_nmap
+cat vmnet8_nmap
+echo "\nBoot2Root server port:"
+cat vmnet8_nmap | grep for | tail -n1 | cut -d" " -f5 > vmnet8_port
 cat vmnet8_port
+echo "\nInstall Dirb soft:"
+sleep 3[s]
 sh dirb_install
 ./dirb https://`cat vmnet8_port`/ dirb222/wordlists/common.txt
